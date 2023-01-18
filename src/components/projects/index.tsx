b8ad6d7ccbff7531d5f0a24d3a5b81edeb07f987
Art Elshani <elshaniart01@gmail.com>
2023-01-18 15:49:38 +0100

    getting image from graphql query and replaced static image with gatsby image

--- src/components/projects/index.tsx
+++ src/components/projects/index.tsx
@@ -1,96 +1,59 @@
 import React from "react";
-import { StaticImage } from "gatsby-plugin-image";
+import { GatsbyImage } from "gatsby-plugin-image";
 import "./projects.css";
 
-const Projects = React.forwardRef<HTMLDivElement | null>((props, ref) => {
-  return (
-    <>
-      <div className="projects-gap" ref={ref}></div>
-      <div className="projects">
-        <h1 className="projects-header">PROJECTS</h1>
-        <div className="projects-header-bottom-line"></div>
-        <p className="about-text">
-          Here you will find some of the projects I've developed using React and
-          React-based Frameworks
-        </p>
-        <div className="projects-text-bottom-line"></div>
-        <div className="projects-container">
-          <div className="project-container">
-            <div className="projects-image-container">
-              <StaticImage
-                src={"../../assets/images/tauto.PNG"}
-                alt="tauto page"
-                className={"projects-image"}
-                layout="constrained"
-              />
-            </div>
-            <div className="projects-text-container">
-              <h2 className="project-header">TAUTO Web Page</h2>
-              <p className="project-text">
-                This is the Tauto Web Page which I created with React. It
-                features a Home Page, About Page, Blog, Services, and Solutions
-                tabs. The client portal, through which Tauto's clients will be
-                able to submit tickets is currently in the works!
-              </p>
-              <a href="https://www.tauto.com/" className="project-button">
-                Visit Site
-              </a>
-            </div>
-          </div>
-          <div className="project-container">
-            <div className="projects-image-container">
-              <StaticImage
-                src={"../../assets/images/simply-recipes.PNG"}
-                alt="tauto page"
-                className={"projects-image"}
-                layout="constrained"
-              />
-            </div>
-            <div className="projects-text-container">
-              <h2 className="project-header">Simply Recipes</h2>
-              <p className="project-text">
-                This is a web application I created while learning Gatsby. The
-                data displayed on the app (recipes, images, tags), are all
-                fetched from Contentful. The routing for the recipes is made
-                programmatically using the folder structure
-              </p>
-              <a
-                href="https://simply-recipes-gatsby-esh.netlify.app/"
-                className="project-button"
-              >
-                Visit Site
-              </a>
-            </div>
-          </div>
-          <div className="project-container">
-            <div className="projects-image-container">
-              <StaticImage
-                src={"../../assets/images/amazon-clone.PNG"}
-                alt="tauto page"
-                className={"projects-image"}
-                layout="constrained"
-              />
-            </div>
-            <div className="projects-text-container">
-              <h2 className="project-header">Amazon Clone</h2>
-              <p className="project-text">
-                This is an Amazon front page clone which I made using React,
-                styled with Tailwind. The data displayed on the app is fetched
-                from the fake store API. I created this app in order to
-                strengthen my understanding of fetching data from an API.
-              </p>
-              <a
-                href="https://amazon-ten-ivory.vercel.app/"
-                className="project-button"
-              >
-                Visit Site
-              </a>
-            </div>
+type ProjectsProps = {
+  projectData: Array<{
+    name: string;
+    childImageSharp: { gatsbyImageData: object };
+    title?: string;
+    text?: string;
+    url?: string;
+  }>;
+};
+
+const Projects = React.forwardRef<HTMLDivElement | null, ProjectsProps>(
+  (props, ref) => {
+    const { projectData } = props;
+    return (
+      <>
+        <div className="projects-gap" ref={ref}></div>
+        <div className="projects">
+          <h1 className="projects-header">PROJECTS</h1>
+          <div className="projects-header-bottom-line"></div>
+          <p className="about-text">
+            Here you will find some of the projects I've developed using React
+            and React-based Frameworks
+          </p>
+          <div className="projects-text-bottom-line"></div>
+          <div className="projects-container">
+            {projectData.map((project, index) => {
+              const { name, text, title, url, childImageSharp } = project;
+
+              return (
+                <div className="project-container" key={index}>
+                  <div className="projects-image-container">
+                    <GatsbyImage
+                      image={childImageSharp?.gatsbyImageData}
+                      alt={name}
+                      className={"projects-image"}
+                    />
+                  </div>
+                  <div className="projects-text-container">
+                    <h2 className="project-header">{title}</h2>
+                    <p className="project-text">{text}</p>
+                    <a href={url} className="project-button">
+                      Visit Site
+                    </a>
+                  </div>
+                </div>
+              );
+            })}
           </div>
         </div>
-      </div>
-    </>
-  );
-});
+      </>
+    );
+  }
+);
 
 export default Projects;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,12 +4,71 @@ import ContactMe from "../components/contact";
 import HeroBanner from "../components/hero";
 import Layout from "../components/layout";
 import Projects from "../components/projects";
+import { graphql, useStaticQuery } from "gatsby";
+
+const query = graphql`
+  query {
+    allFile(
+      filter: { name: { in: ["amazon-clone", "simply-recipes", "tauto"] } }
+    ) {
+      nodes {
+        name
+        childImageSharp {
+          gatsbyImageData(layout: CONSTRAINED, placeholder: BLURRED)
+        }
+      }
+    }
+  }
+`;
 
 export default function Home() {
   const heroRef = React.useRef<null | HTMLDivElement>(null);
   const aboutRef = React.useRef<null | HTMLDivElement>(null);
   const projectsRef = React.useRef<null | HTMLDivElement>(null);
   const contactRef = React.useRef<null | HTMLDivElement>(null);
+
+  const {
+    allFile: { nodes: images },
+  } = useStaticQuery(query);
+  console.log({ images });
+
+  const projectData: Array<object> = [];
+
+  images.forEach((image) => {
+    const project: {
+      name?: string;
+      childImageSharp?: object;
+      title?: string;
+      text?: string;
+      url?: string;
+    } = {};
+
+    project.name = image.name;
+    project.childImageSharp = image.childImageSharp;
+    switch (image.name) {
+      case "amazon-clone":
+        console.log("here");
+        project.title = "Amazon Clone";
+        project.text =
+          "This is an Amazon front page clone which I made using React, styled with Tailwind. The data displayed on the app is fetched from the fake store API. I created this app in order to strengthen my understanding of fetching data from an API.";
+        project.url = "https://amazon-ten-ivory.vercel.app/";
+        break;
+      case "simply-recipes":
+        project.title = "Simply Recipes";
+        project.text =
+          "This is a web application I created while learning Gatsby. The data displayed on the app (recipes, images, tags), are all fetched from Contentful. The routing for the recipes is made programmatically using the folder structure.";
+        project.url = "https://simply-recipes-gatsby-esh.netlify.app/";
+        break;
+      case "tauto":
+        project.title = "TAUTO Web Page";
+        project.text =
+          "This is the Tauto Web Page which I created with React. It features a Home Page, About Page, Blog, Services, and Solutions tabs. The client portal, through which Tauto's clients will be able to submit tickets is currently in the works!";
+        project.url = "https://www.tauto.com/";
+        break;
+    }
+
+    projectData.push(project);
+  });
 
   const scrollControl = (location: string) => {
     switch (location) {
@@ -35,7 +94,7 @@ export default function Home() {
       <main>
         <HeroBanner ref={heroRef} />
         <AboutMe ref={aboutRef} />
-        <Projects ref={projectsRef} />
+        <Projects ref={projectsRef} projectData={projectData} />
         <ContactMe ref={contactRef} />
       </main>
     </Layout>
